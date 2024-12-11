7a9cf8f5102259e91940f468c7a66a52d40f3be9
Ankith <ankithbalu@gmail.com>
2024-12-11 17:46:13 -0500

    Updated version

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Dockerized Node.js App Ankith');
+    res.send('Hello, Dockerized Node.js App Ankith-991677867');
 });
 
 app.listen(PORT, () => {
